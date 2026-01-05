11670f7bca923a4a57c3c297dade01c7ca5a326f
Василий Николаев <tabaqui.vn@gmail.com>
2026-01-05 12:48:08 +0300

    http request/receive. Got messy

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,8 +9,14 @@
 use defmt::info;
 use defmt::error;
 use embassy_executor::Spawner;
+use embassy_net::DhcpConfig;
 use embassy_net::{
+    dns::DnsSocket,
     tcp::TcpSocket, 
+
+    tcp::client::{TcpClient, TcpClientState},
+    
+    Stack,
     StackResources,
     Runner,
 };
@@ -32,7 +38,7 @@ use esp_hal::{
     timer::timg::TimerGroup
 };
 use {esp_backtrace as _, esp_println as _};
-
+use reqwless::client::{HttpClient, TlsConfig};
 extern crate alloc;
 
 macro_rules! mk_static {
@@ -57,9 +63,12 @@ async fn main(spawner: Spawner) -> ! {
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
-    
-    esp_alloc::heap_allocator!(#[ram(reclaimed)] size: 64 * 1024);
-    esp_alloc::heap_allocator!(size: 36 * 1024);
+
+
+    esp_alloc::heap_allocator!(size: 128 * 1024); 
+    // esp_alloc::heap_allocator!(#[ram(reclaimed)] size: 64 * 1024);
+    // esp_alloc::heap_allocator!(size: 36 * 1024);
+    // esp_alloc::heap_allocator!(#[unsafe(link_section = ".dram2_uninit")] size: 64 * 1024);
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     let sw_interrupt =
@@ -75,17 +84,18 @@ async fn main(spawner: Spawner) -> ! {
     controller.set_power_saving(esp_radio::wifi::PowerSaveMode::Minimum).unwrap();
     let wifi_interface = interfaces.sta;
 
-    let config = embassy_net::Config::dhcpv4(Default::default());
+    let config = embassy_net::Config::dhcpv4(DhcpConfig::default());
 
     let rng = Rng::new();
     let seed = (rng.random() as u64) << 32 | rng.random() as u64;
+    let tls_seed = rng.random() as u64 | ((rng.random() as u64) << 32);
 
 
     // Init network stack
     let (stack, runner) = embassy_net::new(
         wifi_interface,
         config,
-        mk_static!(StackResources<3>, StackResources::<3>::new()),
+        mk_static!(StackResources<5>, StackResources::<5>::new()),
         seed,
     );
 
@@ -132,43 +142,46 @@ async fn main(spawner: Spawner) -> ! {
 
         // }
         // break;
-        let mut buf = [0; 1024];
-        loop {
-            // use embedded_io_async::Write;
-            let r = socket
-                .write(b"GET / HTTP/1.0\r\nHost: www.mobile-j.de\r\n\r\n")
-                .await;
-            if let Err(e) = r {
-                error!("write error: {:?}", e);
-                break;
-            }
-            let n = match socket.read(&mut buf).await {
-                Ok(0) => {
-                    info!("read EOF");
-                    break;
-                }
-                Ok(n) => n,
-                Err(e) => {
-                    error!("read error: {:?}", e);
-                    break;
-                }
-            };
-            info!("{}", core::str::from_utf8(&buf[..n]).unwrap());
-        }
-        Timer::after(Duration::from_millis(3000)).await;
-    }
-
-
-    // TODO: Spawn some tasks
-    let _ = spawner;
-
-    loop {
-        info!("Hello world!");
+        access_website(stack, tls_seed).await;
         Timer::after(Duration::from_secs(1)).await;
-    }
+
+    //     let mut buf = [0; 1024];
+    //     loop {
+    //         // use embedded_io_async::Write;
+    //         let r = socket
+    //             .write(b"GET / HTTP/1.0\r\nHost: aeknyy-de.duckdns.org\r\n\r\n")
+    //             .await;
+    //         if let Err(e) = r {
+    //             error!("write error: {:?}", e);
+    //             break;
+    //         }
+    //         let n = match socket.read(&mut buf).await {
+    //             Ok(0) => {
+    //                 info!("read EOF");
+    //                 break;
+    //             }
+    //             Ok(n) => n,
+    //             Err(e) => {
+    //                 error!("read error: {:?}", e);
+    //                 break;
+    //             }
+    //         };
+    //         info!("{}", core::str::from_utf8(&buf[..n]).unwrap());
+    //     }
+    //     Timer::after(Duration::from_millis(3000)).await;
+    // }
+
+
+    // // TODO: Spawn some tasks
+    // let _ = spawner;
+
+    // loop {
+    //     info!("Hello world!");
+    //     Timer::after(Duration::from_secs(1)).await;
+    // }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
-}
+}}
 
 #[embassy_executor::task]
 async fn connection(mut controller: WifiController<'static>) {
@@ -222,3 +235,119 @@ async fn connection(mut controller: WifiController<'static>) {
 async fn net_task(mut runner: Runner<'static, WifiDevice<'static>>) {
     runner.run().await
 }
+
+async fn access_website(stack: Stack<'_>, tls_seed: u64) {
+    let mut rx_buffer = [0; 16386];
+    let mut tx_buffer = [0; 4096];
+    let dns = DnsSocket::new(stack);
+    let tcp_state = TcpClientState::<1, 4096, 16386>::new();
+    let tcp = TcpClient::new(stack, &tcp_state);
+
+    let tls = TlsConfig::new(
+        tls_seed,
+        &mut rx_buffer,
+        &mut tx_buffer,
+        reqwless::client::TlsVerify::None,
+    );
+
+    let mut client = HttpClient::new_with_tls(&tcp, &dns, tls);
+    let mut buffer = [0u8; 16386];
+    // let mut http_req = client
+    //     .request(
+    //         reqwless::request::Method::GET,
+    //         "https://aeknyy-de.duckdns.org/",
+    //     )
+    //     .await
+    //     .unwrap();
+    // let response = http_req.send(&mut buffer).await;
+    loop {
+        let mut http_req = match client
+        .request(
+            reqwless::request::Method::GET,
+            "https://aeknyy-de.duckdns.org/",
+        )
+        .await {
+            Ok(a) => a,
+            Err(_) => {
+                Timer::after(Duration::from_secs(1)).await;
+                info!("Continuing request");
+                break;
+            },
+        };
+        
+
+        loop {
+
+            Timer::after(Duration::from_secs(1)).await;
+
+            let respose = match http_req.send(&mut buffer).await {
+                Ok(a) => a,
+                Err(_) => {
+                    // Timer::after(Duration::from_secs(1)).await;
+                    info!("Continuing send");
+                    break;
+                },
+            };
+            
+            info!("{:?}", respose.status.0);
+            // Timer::after(Duration::from_secs(1)).await;
+            
+            // info!("Reading");
+            // let answer = respose.body().read_to_end().await;
+            // info!("Have been read");
+            // let answer = match answer {
+            //     Ok(a) => info!("Done"),
+            //     Err(_) => {
+            //         error!("Error reading!");
+            //     },
+            // };
+            // info!("{}", core::str::from_utf8(&buffer[..(respose.content_length)]).unwrap());
+            // let s_c = &respose.status.0;
+            // info!("Got response {:?}", s_c);
+
+            // loop {
+                // match respose.body().read_to_end().await {
+                //     Ok(a) => info!("{:?}", a),
+                //     Err(_) => {
+                //         info!("Continuing read");
+                //         continue
+                //     },
+                // }
+            // };
+            
+
+            
+            break;
+
+        }
+        Timer::after(Duration::from_secs(1)).await;
+        
+
+        
+    }
+}
+
+    
+
+    
+    // loop {
+    //     match response.await {
+    //         // Ok(0) => break,  // Connection closed
+    //         Ok(r) => {
+                
+    //             // Optionally parse/print response
+    //             info!("Read {:?}", r.);
+    //         }
+    //         Err(embedded_tls::TlsError::WouldBlock) => {
+    //             // Normal in non-blocking mode — just try again
+    //             continue;
+    //         }
+    //         Err(e) => return Err(e),
+    //     }
+    // }
+    // let res = response.body().read_to_end().await.unwrap();
+
+    // let content = core::str::from_utf8(res).unwrap();
+    // info!("{}", content);
+// }
+
