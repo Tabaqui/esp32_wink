feda81c3511b4ec2a4e976b7e1787d0554fdf7bb
Василий Николаев <tabaqui.vn@gmail.com>
2026-01-12 10:09:23 +0300

    Mqtt connect workin

--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,28 @@
 )]
 mod mqtt;
 
-use defmt::info;
+// use embedded_nal_async::Dns;
+
+use alloc::boxed::Box;
 use defmt::error;
+use defmt::info;
 use embassy_executor::Spawner;
 use embassy_net::DhcpConfig;
 use embassy_net::{
+    Runner, Stack, StackResources,
     dns::DnsSocket,
-    tcp::TcpSocket, 
-
+    tcp::TcpSocket,
     tcp::client::{TcpClient, TcpClientState},
-    
-    Stack,
-    StackResources,
-    Runner,
-};
-use esp_radio::wifi::{
-    ClientConfig,
-    ModeConfig,
-    ScanConfig,
-    WifiController,
-    WifiDevice,
-    WifiEvent,
-    WifiStaState,
 };
 use embassy_time::{Duration, Timer};
-use esp_hal::{
-    
-    clock::CpuClock,
-    ram,
-    rng::Rng,
-    timer::timg::TimerGroup
+use esp_hal::riscv::asm::nop;
+use esp_hal::{clock::CpuClock, ram, rng::Rng, timer::timg::TimerGroup};
+use esp_radio::wifi::{
+    ClientConfig, ModeConfig, ScanConfig, WifiController, WifiDevice, WifiEvent, WifiStaState,
 };
+use static_cell::StaticCell;
 use {esp_backtrace as _, esp_println as _};
-use reqwless::client::{HttpClient, TlsConfig};
+// use reqwless::client::{HttpClient, TlsConfig};
 extern crate alloc;
 
 macro_rules! mk_static {
@@ -65,8 +54,7 @@ async fn main(spawner: Spawner) -> ! {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
-
-    esp_alloc::heap_allocator!(size: 128 * 1024); 
+    esp_alloc::heap_allocator!(size: 128 * 1024);
     // esp_alloc::heap_allocator!(#[ram(reclaimed)] size: 64 * 1024);
     // esp_alloc::heap_allocator!(size: 36 * 1024);
     // esp_alloc::heap_allocator!(#[unsafe(link_section = ".dram2_uninit")] size: 64 * 1024);
@@ -78,11 +66,13 @@ async fn main(spawner: Spawner) -> ! {
 
     info!("Embassy initialized!");
 
-    let cr = mk_static!(esp_radio::Controller<'static> , esp_radio::init().unwrap());
+    let cr = mk_static!(esp_radio::Controller<'static>, esp_radio::init().unwrap());
     let (mut controller, interfaces) =
         esp_radio::wifi::new(&*cr, peripherals.WIFI, Default::default()).unwrap();
 
-    controller.set_power_saving(esp_radio::wifi::PowerSaveMode::Minimum).unwrap();
+    controller
+        .set_power_saving(esp_radio::wifi::PowerSaveMode::Maximum)
+        .unwrap();
     let wifi_interface = interfaces.sta;
 
     let config = embassy_net::Config::dhcpv4(DhcpConfig::default());
@@ -90,7 +80,6 @@ async fn main(spawner: Spawner) -> ! {
     let rng = Rng::new();
     let seed = (rng.random() as u64) << 32 | rng.random() as u64;
     let tls_seed = rng.random() as u64 | ((rng.random() as u64) << 32);
-
 
     // Init network stack
     let (stack, runner) = embassy_net::new(
@@ -100,12 +89,12 @@ async fn main(spawner: Spawner) -> ! {
         seed,
     );
 
+
     spawner.spawn(connection(controller)).ok();
     spawner.spawn(net_task(runner)).ok();
-    // spawner.spawn(mqtt::mqtt_task());
 
-    let mut rx_buffer = [0; 4096];
-    let mut tx_buffer = [0; 4096];
+    let rx_buffer =  mk_static!([u8; 4096], [0; 4096]);
+    let tx_buffer = Box::leak(Box::new([0; 4096]));
 
     loop {
         if stack.is_link_up() {
@@ -123,67 +112,37 @@ async fn main(spawner: Spawner) -> ! {
         Timer::after(Duration::from_millis(500)).await;
     }
 
+    // loop {
+    Timer::after(Duration::from_millis(1_000)).await;
+
+    let mut socket = TcpSocket::new(stack, rx_buffer, tx_buffer);
+
+    socket.set_timeout(Some(Duration::from_secs(100)));
+
+    let remote_endpoint = (core::net::Ipv4Addr::new(192, 168, 1, 1), 1883);
+    info!("connecting...");
     loop {
-        Timer::after(Duration::from_millis(1_000)).await;
-
-        let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
-
-        socket.set_timeout(Some(Duration::from_secs(10)));
-
-        let remote_endpoint = (core::net::Ipv4Addr::new(142, 250, 185, 115), 80);
-        info!("connecting...");
         let r = socket.connect(remote_endpoint).await;
         if let Err(e) = r {
             error!("connect error: {:?}", e);
-            continue;
-        }
-        // loop {
         
-            info!("connected!");
-            // Timer::after(Duration::from_secs(10)).await
+        } else {
+            break;
+        }
+    }
 
-        // }
-        // break;
-        access_website(stack, tls_seed).await;
+    info!("connected!");
+    Timer::after(Duration::from_secs(1)).await;
+    // break;
+
+    mqtt::mqtt_task(socket).await;
+
+    loop {
+        info!("Got waiting");
         Timer::after(Duration::from_secs(1)).await;
-
-    //     let mut buf = [0; 1024];
-    //     loop {
-    //         // use embedded_io_async::Write;
-    //         let r = socket
-    //             .write(b"GET / HTTP/1.0\r\nHost: aeknyy-de.duckdns.org\r\n\r\n")
-    //             .await;
-    //         if let Err(e) = r {
-    //             error!("write error: {:?}", e);
-    //             break;
-    //         }
-    //         let n = match socket.read(&mut buf).await {
-    //             Ok(0) => {
-    //                 info!("read EOF");
-    //                 break;
-    //             }
-    //             Ok(n) => n,
-    //             Err(e) => {
-    //                 error!("read error: {:?}", e);
-    //                 break;
-    //             }
-    //         };
-    //         info!("{}", core::str::from_utf8(&buf[..n]).unwrap());
-    //     }
-    //     Timer::after(Duration::from_millis(3000)).await;
-    // }
-
-
-    // // TODO: Spawn some tasks
-    // let _ = spawner;
-
-    // loop {
-    //     info!("Hello world!");
-    //     Timer::after(Duration::from_secs(1)).await;
-    // }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
-}}
+        
+    }
+}
 
 #[embassy_executor::task]
 async fn connection(mut controller: WifiController<'static>) {
@@ -193,9 +152,7 @@ async fn connection(mut controller: WifiController<'static>) {
         match esp_radio::wifi::sta_state() {
             WifiStaState::Connected => {
                 // wait until we're no longer connected
-                controller
-                    .wait_for_event(WifiEvent::StaDisconnected)
-                    .await;
+                controller.wait_for_event(WifiEvent::StaDisconnected).await;
                 Timer::after(Duration::from_millis(5000)).await
             }
             _ => {}
@@ -237,119 +194,3 @@ async fn connection(mut controller: WifiController<'static>) {
 async fn net_task(mut runner: Runner<'static, WifiDevice<'static>>) {
     runner.run().await
 }
-
-async fn access_website(stack: Stack<'_>, tls_seed: u64) {
-    let mut rx_buffer = [0; 16386];
-    let mut tx_buffer = [0; 4096];
-    let dns = DnsSocket::new(stack);
-    let tcp_state = TcpClientState::<1, 4096, 16386>::new();
-    let tcp = TcpClient::new(stack, &tcp_state);
-
-    let tls = TlsConfig::new(
-        tls_seed,
-        &mut rx_buffer,
-        &mut tx_buffer,
-        reqwless::client::TlsVerify::None,
-    );
-
-    let mut client = HttpClient::new_with_tls(&tcp, &dns, tls);
-    let mut buffer = [0u8; 16386];
-    // let mut http_req = client
-    //     .request(
-    //         reqwless::request::Method::GET,
-    //         "https://aeknyy-de.duckdns.org/",
-    //     )
-    //     .await
-    //     .unwrap();
-    // let response = http_req.send(&mut buffer).await;
-    loop {
-        let mut http_req = match client
-        .request(
-            reqwless::request::Method::GET,
-            "https://aeknyy-de.duckdns.org/",
-        )
-        .await {
-            Ok(a) => a,
-            Err(_) => {
-                Timer::after(Duration::from_secs(1)).await;
-                info!("Continuing request");
-                break;
-            },
-        };
-        
-
-        loop {
-
-            Timer::after(Duration::from_secs(1)).await;
-
-            let respose = match http_req.send(&mut buffer).await {
-                Ok(a) => a,
-                Err(_) => {
-                    // Timer::after(Duration::from_secs(1)).await;
-                    info!("Continuing send");
-                    break;
-                },
-            };
-            
-            info!("{:?}", respose.status.0);
-            // Timer::after(Duration::from_secs(1)).await;
-            
-            // info!("Reading");
-            // let answer = respose.body().read_to_end().await;
-            // info!("Have been read");
-            // let answer = match answer {
-            //     Ok(a) => info!("Done"),
-            //     Err(_) => {
-            //         error!("Error reading!");
-            //     },
-            // };
-            // info!("{}", core::str::from_utf8(&buffer[..(respose.content_length)]).unwrap());
-            // let s_c = &respose.status.0;
-            // info!("Got response {:?}", s_c);
-
-            // loop {
-                // match respose.body().read_to_end().await {
-                //     Ok(a) => info!("{:?}", a),
-                //     Err(_) => {
-                //         info!("Continuing read");
-                //         continue
-                //     },
-                // }
-            // };
-            
-
-            
-            break;
-
-        }
-        Timer::after(Duration::from_secs(1)).await;
-        
-
-        
-    }
-}
-
-    
-
-    
-    // loop {
-    //     match response.await {
-    //         // Ok(0) => break,  // Connection closed
-    //         Ok(r) => {
-                
-    //             // Optionally parse/print response
-    //             info!("Read {:?}", r.);
-    //         }
-    //         Err(embedded_tls::TlsError::WouldBlock) => {
-    //             // Normal in non-blocking mode — just try again
-    //             continue;
-    //         }
-    //         Err(e) => return Err(e),
-    //     }
-    // }
-    // let res = response.body().read_to_end().await.unwrap();
-
-    // let content = core::str::from_utf8(res).unwrap();
-    // info!("{}", content);
-// }
-
--- src/mqtt.rs
+++ src/mqtt.rs
@@ -1,29 +1,92 @@
+// use core::ptr::read_volatile;
+
+use alloc::boxed::Box;
+// use alloc::boxed::Box;
+use defmt::{error, info};
 use embassy_executor::task;
 use embassy_net::tcp::TcpSocket;
-
-use esp_hal::rng::Rng;
-use rust_mqtt::client::{client::MqttClient, client_config::{ClientConfig, MqttVersion}};
+use embassy_time::{Duration, Timer};
+use rust_mqtt::{
+    buffer:: BumpBuffer, 
+    client::{Client, options::{ConnectOptions, WillOptions}}, 
+    config::{KeepAlive, SessionExpiryInterval}, 
+    types::{MqttBinary, MqttString, QoS}};
 use smoltcp::socket;
+
 use {esp_backtrace as _, esp_println as _};
 
 // use embedded_nal_async::TcpConnect;
-
-
-#[task]
+macro_rules! mk_static {
+    ($t:ty,$val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        #[deny(unused_attributes)]
+        let x = STATIC_CELL.uninit().write(($val));
+        x
+    }};
+}
+// #[task]
 pub async fn mqtt_task(socket: TcpSocket<'static>) {
-    // TcpSocket
-    // let mut a_buf = AllocBuffer;
-    let mut s_buff = [0; 1024];
-    let ss = s_buff.len().clone();
-    let mut r_buff = [0; 1024];
-    let rr = r_buff.len().clone();
+    // let mut buf = [0u8; 1024];
 
-    MqttClient::<_, _, _>::new(
-        socket,
-        &mut s_buff,
-        ss,
-        &mut r_buff, 
-        rr, 
-        ClientConfig::<5, Rng>::new(MqttVersion::MQTTv5, Rng::new())
-    );
+    // let s = Box::new(socket);
+    // let stb = mk_static!([u8; 1024], buf);
+    // let socket_ = mk_static!(TcpSocket, socket);
+    let stb2 = mk_static!([u8; 1024], [0; 1024]);
+    let y = BumpBuffer::new( stb2);
+    let b = mk_static!(BumpBuffer, y);
+
+    // let mut buf = [0u8; 1024];
+    // let x = Box::new(BumpBuffer::new(stb));
+
+    // let b_buff: &'static mut BumpBuffer = Box::leak(x);
+
+    let mut client = Client::<
+        'static,
+        TcpSocket<'static>,
+        BumpBuffer<'static>, 
+        1, 
+        1, 
+        1
+        >
+        ::new(b);
+
+    let o = ConnectOptions {
+                session_expiry_interval: SessionExpiryInterval::Seconds(5),
+                clean_start: false,
+                keep_alive: KeepAlive::Seconds(3),
+                will: Some(WillOptions {
+                    will_qos: QoS::ExactlyOnce,
+                    will_retain: true,
+                    will_topic: MqttString::try_from("dead").unwrap(),
+                    will_payload: MqttBinary::try_from("joe mama").unwrap(),
+                    will_delay_interval: 10,
+                    is_payload_utf8: true,
+                    message_expiry_interval: Some(20),
+                    content_type: Some(MqttString::try_from("txt").unwrap()),
+                    response_topic: None,
+                    correlation_data: None,
+                }),
+                user_name: Some(MqttString::try_from("test").unwrap()),
+                password: Some(MqttBinary::try_from("testPass").unwrap()),
+            };
+
+    
+    // loop {
+    Timer::after(Duration::from_secs(1)).await;
+
+    let c_info = client.connect(
+        socket, 
+        &o, 
+        Some(MqttString::try_from("rust-mqtt-demo-client").unwrap()
+        )
+    )
+    .await;
+    match c_info {
+        Ok(_) => info!("Mqtt connected"),
+        Err(e) => {
+            error!("Mqtt not connected");
+        }
+    };
+    // }
+    
 }
